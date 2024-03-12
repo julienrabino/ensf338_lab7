52c6e0870f7c00d138706abfc8bb2b5693fc7e8c
brikhi2027 <bhavna.rikhi@ucalgary.ca>
2024-03-12 14:46:10 -0600

    started ex3

---
+++ ex3.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self,data, parent = None, left = None, right = None):
+        self.data = data
+        self.parent = parent
+        self.left = left
+        self.right = right
+        self.balance = 0
