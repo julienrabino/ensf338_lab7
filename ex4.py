7efe5da74ef9d5b0321743c81c62495b903d7340
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2024-03-12 14:24:07 -0600

    template for ex4

---
+++ ex4.py
@@ -0,0 +1,22 @@
+# code from ex3 goes here
+
+class binarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    # methods from number 3 go here
+        
+    def _lr_rotate(self, pivot):
+
+    def _rl_rotate(self, pivot):
+
+    def insert():
+
+
+
+
+
+# 4. Create two extra test cases, both testing case 3b (now the code should
+# not return an error!) [0.2 pts]
+
+       
