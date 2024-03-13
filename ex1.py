abf9cb873e4df6aa84cf845c7ae9534eea7833a1
Julien Rabino <julienmescha.rabino@ucalgary.ca>
2024-03-12 23:53:51 -0600

    finished 1 and then almost done 2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,6 @@
+import random
+import timeit
+import matplotlib.pyplot as plt
 class Node:
     def __init__(self,data, parent = None, left = None, right = None):
         self.data = data
@@ -59,20 +62,78 @@ class binarySearchTree:
     
     def height(self, node):
         if node is None:
-            return -1
-        else:
-            height = -1
-            stack = []  
-            stack.append(node)
-            stack.append(height)
-            while len(stack)>0:
-                current_height = stack.pop()
-                current = stack.pop()
-                if current is not None:
-                    height = max(height, current_height)
-                    stack.append(current.left)
-                    stack.append(current_height + 1)
-                    stack.append(current.right)
-                    stack.append(current_height + 1)
-            return height
+            return 0
+    
+        queue = [node]
+        height = 0
+    
+        while len(queue)>0:
+            size = len(queue)
+        
+            for i in range(size):
+                current = queue.pop(0)
+            
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+        
+            height += 1
+    
+        return height
 
+
+
+
+    def largest_balance(self):
+        if self.root is None:
+            return 0 
+        
+        balance_list = []
+        
+        stack = [self.root]
+        
+        while len(stack)>0:
+            node = stack.pop()
+            balance = abs(node.balance)
+            balance_list.append(balance)
+
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        
+        max_balance = max(balance_list)
+        
+        return max_balance
+
+def main():
+    arr = []
+    times = []
+    largest_abs_balances = []
+
+    for i in range(1000):
+        arr.append(i)
+
+   
+
+    for i in range(1000):
+        random.shuffle(arr) # shuffling arr again
+        BST = binarySearchTree()
+        for num in arr:
+            BST.insert(num)
+
+        random_value = random.choice(arr)
+
+        times.append(timeit.timeit(lambda: BST.search(random_value), number=10)/10)
+        largest_abs_balances.append(BST.largest_balance())
+        
+
+    plt.scatter(largest_abs_balances, times)
+    plt.title('Search time vs Absolute Balance')
+    plt.xlabel('Absolute Balance')
+    plt.ylabel('Search Time')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
--- ex2.py
+++ ex2.py
@@ -1,3 +1,6 @@
+import random
+import timeit
+import matplotlib.pyplot as plt
 class Node:
     def __init__(self,data, parent = None, left = None, right = None):
         self.data = data
@@ -10,9 +13,11 @@ class Node:
 class binarySearchTree:
     def __init__(self):
         self.root = None
+        self.pivot = None
     
     def insert(self, data):
         current = self.root
+
         parent = None
 
         while current is not None:
@@ -48,9 +53,61 @@ class binarySearchTree:
         return None
     
     def update_balances(self, node):
+        self.pivot = None
+        node_inserted = node
+        parent = node.parent
+
         while node != None:
+            if node.balance >=1 or node.balance<=-1:
+                if self.pivot is None:
+                    self.pivot = node
+            
             node.balance = self.calculate_balance(node)
             node = node.parent
+
+            
+        if self.pivot is None:
+            print("Case 1: No pivot found")
+        elif self.pivot is not None and (self.pivot.balance >= 1 or self.pivot.balance <= -1):
+            print("Case 2: Pivot exists and the node was aded to the shorter subtree")
+
+            if self.pivot.balance >1 and node_inserted.data<self.pivot.data:
+                if parent.left == node_inserted:
+                    parent.left == None
+                else:
+                    parent.right == None
+                self.insert_to_shorter_subtree(node_inserted, 'left')
+            elif self.pivot.balance<-1 and node_inserted.data> self.pivot.data:
+                if parent.left == node_inserted:
+                    parent.left == None
+                else:
+                    parent.right == None
+                self.insert_to_shorter_subtree(node_inserted, 'right')
+
+
+    def insert_to_shorter_subtree(self, node, subtree):
+        current = self.pivot
+        if subtree == 'left':
+            current = current.left
+        else:
+            current = current.right
+            
+        while current is not None:
+            parent = current
+            if node.data <= current.data:
+                current.balance -=1
+                current = current.left
+            else:
+                current.balance +=1
+                current = current.right
+
+
+        if node.data <= parent.data:
+            parent.left = node
+        else:
+            parent.right = node
+        
+        
 
     def calculate_balance(self,node):
         left_height = self.height(node.left)
@@ -59,20 +116,71 @@ class binarySearchTree:
     
     def height(self, node):
         if node is None:
-            return -1
-        else:
-            height = -1
-            stack = []  
-            stack.append(node)
-            stack.append(height)
-            while len(stack)>0:
-                current_height = stack.pop()
-                current = stack.pop()
-                if current is not None:
-                    height = max(height, current_height)
-                    stack.append(current.left)
-                    stack.append(current_height + 1)
-                    stack.append(current.right)
-                    stack.append(current_height + 1)
-            return height
+            return 0
+    
+        queue = [node]
+        height = 0
+    
+        while len(queue)>0:
+            size = len(queue)
+        
+            for i in range(size):
+                current = queue.pop(0)
+            
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+        
+            height += 1
+    
+        return height
 
+
+
+
+    def largest_balance(self):
+        if self.root is None:
+            return 0 
+        
+        balance_list = []
+        
+        stack = [self.root]
+        
+        while len(stack)>0:
+            node = stack.pop()
+            balance = abs(node.balance)
+            balance_list.append(balance)
+
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        
+        max_balance = max(balance_list)
+        
+        return max_balance
+    
+    def _print_tree_recursive(self, node):
+        if node is not None:
+            # Print current node and its balance
+            print(f"Node: {node.data}, Balance: {node.balance}")
+
+            # Recursively print left subtree
+            self._print_tree_recursive(node.left)
+
+            # Recursively print right subtree
+            self._print_tree_recursive(node.right)
+def main():
+    BST = binarySearchTree()
+    BST.insert(1)
+    BST.insert(2)
+    BST.insert(3)
+    BST.insert(5)
+    BST.insert(4)
+
+    BST._print_tree_recursive(BST.root)
+
+
+if __name__ == '__main__':
+    main()
